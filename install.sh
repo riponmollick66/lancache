e6b291523e755d4d9362fecd5d98545851b3e8c9
Ripon Mollick <87811215+riponmollick66@users.noreply.github.com>
2023-07-10 01:33:47 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 /usr/bin/apt-get update	;
-/usr/bin/apt-get install -y bind9* nginx jq curl dnsutils git	;
+/usr/bin/apt-get install -y bind9* nginx-full jq curl dnsutils git	;
 mkdir -p /var/cache/bind /var/log/named && chown bind:bind /var/cache/bind /var/log/named	;
 cp -r /opt/lancache-main/bind/* /etc/bind/	;
 chown -R bind:bind /etc/bind/	;
@@ -8,6 +8,7 @@ rm -rf /etc/nginx/sites-enabled/*
 cp -r /opt/lancache-main/nginx/* /etc/nginx/	;
 ln -s /etc/nginx/sites-available/10_cache.conf /etc/nginx/sites-enabled/10_generic.conf	;
 ln -s /etc/nginx/sites-available/20_upstream.conf /etc/nginx/sites-enabled/20_upstream.conf	;
+ln -s /etc/nginx/stream-available/10_sni.conf /etc/nginx/stream-enabled/10_sni.conf	;
 
 # Start services at boot
 #====#===========#===========
