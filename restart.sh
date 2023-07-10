0ae70ef6322923ec808348cb3e1a2600e9a8a235
Ripon Mollick <87811215+riponmollick66@users.noreply.github.com>
2023-07-10 17:22:26 +0600

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,7 +1,3 @@
-# Start services at boot
-#====#===========#===========
-/bin/systemctl enable bind9 nginx	;
-
 # Restart the services
 #====#===========#===========
 /bin/systemctl restart bind9 nginx	;
