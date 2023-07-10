d057f34d19fa2036171ec9d6052176e445049b6c
Ripon Mollick <87811215+riponmollick66@users.noreply.github.com>
2023-07-11 00:32:58 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ cp -r /opt/lancache-main/bind/* /etc/bind/	;
 chown -R bind:bind /etc/bind/	;
 mkdir -m 755 -p /harddisk/cache /harddisk/logs	;
 mkdir /etc/nginx/stream-enabled  ;
-rm -rf /etc/nginx/sites-enabled/*
+rm -rf /etc/nginx/sites-enabled/default  ;
 cp -r /opt/lancache-main/nginx/* /etc/nginx/	;
 ln -s /etc/nginx/sites-available/10_cache.conf /etc/nginx/sites-enabled/	;
 ln -s /etc/nginx/sites-available/20_upstream.conf /etc/nginx/sites-enabled/	;
-ln -s /etc/nginx/stream-available/10_sni.conf /etc/nginx/stream-enabled/
+ln -s /etc/nginx/stream-available/10_sni.conf /etc/nginx/stream-enabled/  ;
 
 # Start services at boot
 #====#===========#===========
